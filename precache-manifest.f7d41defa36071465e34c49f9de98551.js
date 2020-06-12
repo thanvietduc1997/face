d95973edadd4ad1d4dee3c06a7b97f1ddd29ddd6
thanvietduc1997 <thanvietduc1997@gmail.com>
2020-06-13 02:53:48 +0700

    Updates

---
+++ precache-manifest.f7d41defa36071465e34c49f9de98551.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6f4b4a91b8028b759615b54e3ba655",
+    "url": "/face/index.html"
+  },
+  {
+    "revision": "2cb1f1b27d0a8563549e",
+    "url": "/face/static/css/main.fce0de85.chunk.css"
+  },
+  {
+    "revision": "06dadcf8f85db59a0b4d",
+    "url": "/face/static/js/2.d19435fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face/static/js/2.d19435fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb1f1b27d0a8563549e",
+    "url": "/face/static/js/main.9d83a575.chunk.js"
+  },
+  {
+    "revision": "1ef570bcb7bd6746ae8e",
+    "url": "/face/static/js/runtime-main.6c55c8a7.js"
+  },
+  {
+    "revision": "1915932f844802118ab25287fcd4662c",
+    "url": "/face/static/media/BO.1915932f.jpeg"
+  },
+  {
+    "revision": "c44482b095afb616e8c1c51ca0bd3f42",
+    "url": "/face/static/media/mxnet-logo.c44482b0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c2bd0cf42f494fe8bedb67a66a045da.js"
+  "/face/precache-manifest.f7d41defa36071465e34c49f9de98551.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/face/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
